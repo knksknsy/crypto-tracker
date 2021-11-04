5e9b55cacd63c5c5036d60077494bbeac0b33cce
Kaan Keskinsoy <kk148@hdm-stuttgart.de>
2021-11-04 20:00:12 +0100

    Add responsive design.

--- src/clj/cryptotracker/routes/home.clj
+++ src/clj/cryptotracker/routes/home.clj
@@ -14,6 +14,32 @@
             [clj-time.coerce :as tc]))
 
 (import (org.apache.commons.codec.binary Base64))
+(import (java.awt Font))
+(import (java.awt Toolkit))
+
+(def width 0.55)
+(def height 0.6)
+
+(defn getWidth [percentage]
+  (* 
+    percentage
+    (.getWidth (.getScreenSize (Toolkit/getDefaultToolkit)))
+  )
+)
+
+(defn getHeight [percentage]
+  (if (> (.getHeight (.getScreenSize (Toolkit/getDefaultToolkit))) (.getWidth (.getScreenSize (Toolkit/getDefaultToolkit))))
+    (.getHeight (.getScreenSize (Toolkit/getDefaultToolkit)))
+    (*
+      percentage
+      (.getHeight (.getScreenSize (Toolkit/getDefaultToolkit)))
+    )
+  )
+)
+
+(defn small-plain [size] 
+  (Font. Font/SANS_SERIF Font/PLAIN size)
+)
 
 (defn prepare-data-for-exlist [data]
   (let [coins (vals ((json/read-str (@data :body) :key-fn keyword) :Data))]
@@ -25,7 +51,7 @@
 )
 
 (def apiBaseUrl "https://min-api.cryptocompare.com/data/")
-(def coinsUrl "https://www.cryptocompare.com/api/data/coinlist/")
+(def coinsUrl "https://min-api.cryptocompare.com/data/all/coinlist")
 
 (defn average [numbers]
   (/ (apply + numbers) (count numbers))
@@ -64,13 +90,12 @@
     (c/xy-chart
       {}
       {
-        :width 1050
-        :height 400
+        :width (getWidth width)
+        :height (getHeight height)
         :title "Select crypto currencies to compare"
-        :x-axis { :title "Date" :ticks-visible? true :tick-mark-spacing-hint 20 }
-        :y-axis { :title "Price [$]" :decimal-pattern "######.##" :tick-mark-spacing-hint 20 }
+        :x-axis { :title "Date" :ticks-visible? false }
+        :y-axis { :title "Price [$]" :ticks-visible? false }
         :theme :ggplot2
-        :date-pattern "dd.MM.yyyy"
       }
     )
   )
@@ -78,13 +103,12 @@
     (c/xy-chart
       {}
       {
-        :width 1050
-        :height 400
+        :width (getWidth width)
+        :height (getHeight height)
         :title title
-        :x-axis { :title "Date" :ticks-visible? true :tick-mark-spacing-hint 20 }
-        :y-axis { :title "Price [$]" :decimal-pattern "######.##" :tick-mark-spacing-hint 20 }
+        :x-axis { :title "Date" :ticks-visible? false }
+        :y-axis { :title "Price [$]" :ticks-visible? false }
         :theme :ggplot2
-        :date-pattern "dd.MM.yyyy"
       }
     )
   )
@@ -94,32 +118,34 @@
   (if (and (> (count cur1Closes) 0) (> (count cur2Closes) 0))
     (c/xy-chart
       {
-        (str cur1 " " "\nmin: " (check-min cur1Closes) "\nmax: " (check-max cur1Closes) "\naverage: " (check-average cur1Closes))
+        (str cur1 ":\t" "min: " (check-min cur1Closes) " max: " (check-max cur1Closes) " average: " (check-average cur1Closes))
         {
           :x cur1Times
           :y cur1Closes
           :style { :marker-type :none }
         }
-        (str cur2 "\nmin: " (check-min cur2Closes) "\nmax: " (check-max cur2Closes) "\naverage: " (check-average cur2Closes))
+        (str cur2 ":\t" "min: " (check-min cur2Closes) " max: " (check-max cur2Closes) " average: " (check-average cur2Closes))
         {
           :x cur2Times
           :y cur2Closes
           :style { :marker-type :none }
         }
-        (str cur1 " - " cur2)
-        {
-          :x cur2Times
-          :y (map - cur1Closes cur2Closes)
-          :style { :marker-type :none }
-        }
+        ;; (str cur1 " - " cur2)
+        ;; {
+        ;;   :x cur2Times
+        ;;   :y (map - cur1Closes cur2Closes)
+        ;;   :style { :marker-type :none }
+        ;; }
       }
       {
-        :width 1050
-        :height 400
+        :width (getWidth width)
+        :height (getHeight height)
         :title (str "Comparison of " cur1 " and " cur2)
-        :x-axis { :title "Date" :ticks-visible? true :tick-mark-spacing-hint 20 }
-        :y-axis { :title "Price [$]" :decimal-pattern "######.##" :tick-mark-spacing-hint 20 }
+        :x-axis { :title "Date" :ticks-visible? true :tick-mark-spacing-hint 50 }
+        :y-axis { :title "Price [$]" :decimal-pattern "#,###.##" :tick-mark-spacing-hint 50 }
         :theme :ggplot2
+        :axis { :ticks { :labels { :font (small-plain 8) }}}
+        :legend { :visible? true, :position :inside-nw, :font (small-plain 8) }
         :date-pattern "dd.MM.yyyy"
       }
     )
@@ -176,58 +202,57 @@
   (h/html (app-head)
     (app-body
       [:div {:class "container"}
-        (form-to {:class "form-horizontal"} [:get "/compare"]
-          [:div {:class "form-group"}
-            (label {:class "control-label"} "cur1" "Currency 1")
-            [:select#cur1 {:name "cur1", :class "form-control"}
-              (select-options respCoinlist)
-            ]
+        [:div {:class "row"}
+          [:div {:class "col-lg-10 order-lg-1"}
+            chart-container
           ]
-          [:div {:class "form-group"}
-            (label {:class "control-label"} "cur2" "Currency 2")
-            [:select#cur2 {:name "cur2", :class "form-control"}
-              (select-options respCoinlist)
-            ]
-          ]
-          [:div {:class "row"}
-            [:div {:class "col-6"}
-              [:div {:class "row"}
+          [:div {:class "col-lg-2 order-lg-12"}
+            (form-to {:class "form-horizontal"} [:get "/compare"]
+              [:div {:class "form-group"}
+                (label {:class "control-label"} "cur1" "Currency 1")
+                [:select#cur1 {:name "cur1", :class "form-control"}
+                  (select-options respCoinlist)
+                ]
+              ]
+              [:div {:class "form-group"}
+                (label {:class "control-label"} "cur2" "Currency 2")
+                [:select#cur2 {:name "cur2", :class "form-control"}
+                  (select-options respCoinlist)
+                ]
+              ]
+              [:div {:class "form-group"}
+                (label {:class "control-label"} "range" "Type of range")
                 [:div {:class "col"}
-                  [:div {:class "form-group"}
-                    (label {:class "control-label"} "range" "Type of range")
-                    [:div {:class "radio"}
-                      [:label
-                        [:input#minutes {:type "radio" :name "range" :value "histominute" :class "from-control"}]
-                        "minutes"
-                      ]
-                    ]
-                    [:div {:class "radio"}
-                      [:label
-                        [:input#hours {:type "radio" :name "range" :value "histohour" :class "from-control"}]
-                        "hours"
-                      ]
-                    ]
-                    [:div {:class "radio"}
-                      [:label
-                        [:input#days {:type "radio" :name "range" :value "histoday" :checked "checked" :class "from-control"}]
-                        "days"
-                      ]
+                  [:div {:class "radio"}
+                    [:label
+                      [:input#minutes {:type "radio" :name "range" :value "histominute" :class "from-control"}]
+                      " minutes"
                     ]
                   ]
-                ]
-                [:div {:class "col"}
-                  [:div {:class "form-group"}
-                    (label {:class "control-label"} "limit" "Limit of range")
-                    [:input#limit {:type "number" :min 1 :step 1 :name "limit" :class "form-control" :value 30}]
+                  [:div {:class "radio"}
+                    [:label
+                      [:input#hours {:type "radio" :name "range" :value "histohour" :class "from-control"}]
+                      " hours"
+                    ]
+                  ]
+                  [:div {:class "radio"}
+                    [:label
+                      [:input#days {:type "radio" :name "range" :value "histoday" :checked "checked" :class "from-control"}]
+                      " days"
+                    ]
                   ]
                 ]
               ]
-            ]
-            [:div {:class "form-group col-6"}
-              (submit-button {:class "btn btn-primary float-right"} "Compare")
-            ]
-          ])
-          chart-container
+              [:div {:class "form-group"}
+                (label {:class "control-label"} "limit" "Limit of range")
+                [:input#limit {:type "number" :min 1 :step 1 :name "limit" :class "form-control" :value 30}]
+              ]
+              [:div {:class "form-group"}
+                (submit-button {:class "btn btn-primary"} "Compare")
+              ]
+            )
+          ]
+        ]
       ]
     )
   )
@@ -236,7 +261,7 @@
 (defn home-page [coinList]
   (base-page
     [:div {:class "chart-container"}
-      [:img {:src (str "data:image/svg+xml;base64," (String. (Base64/encodeBase64 (c/to-bytes (empty-chart) :svg)))), :class "img-responsive"}]
+      [:img {:src (str "data:image/svg+xml;base64," (String. (Base64/encodeBase64 (c/to-bytes (empty-chart) :svg)))), :class "img-fluid"}]
     ]
     coinList
   )
@@ -245,7 +270,7 @@
 (defn compare-page [cur1 respCur1Times respCur1Closes cur2 respCur2Times respCur2Closes coinList]
   (base-page
     [:div {:class "chart-container"}
-      [:img {:src (str "data:image/svg+xml;base64," (String. (Base64/encodeBase64 (c/to-bytes (chart cur1 respCur1Times respCur1Closes cur2 respCur2Times respCur2Closes) :svg)))), :class "img-responsive"}]
+      [:img {:src (str "data:image/svg+xml;base64," (String. (Base64/encodeBase64 (c/to-bytes (chart cur1 respCur1Times respCur1Closes cur2 respCur2Times respCur2Closes) :svg)))), :class "img-fluid"}]
     ]
     coinList
   )
